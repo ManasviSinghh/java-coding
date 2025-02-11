c8070b8bf99f565655a0643c274a391fc4a70f33
Manasvi Singh <shivamanasvi5@gmail.com>
2025-02-11 19:08:40 +0530

    Add files via upload

---
+++ code4.java
@@ -0,0 +1,11 @@
+public class code4 {
+    public static void main(String[] args) {
+        byte b = 8;
+        System.out.println(b);
+        char ch = 'a';
+        System.out.println(ch);
+        boolean var = true;
+        System.out.println(var);
+    }
+    
+}

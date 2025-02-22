50b7d94e2af3db54edbab288f4bb0d7ed616b39e
Manasvi Singh <2200560100059@bbdniit.ac.in>
2025-02-22 23:28:52 +0530

    Add files via upload

---
+++ code9.java
@@ -0,0 +1,10 @@
+public class code9 {//unary operator
+    public static void main(String[] args) {
+        int a = 4;
+        int b = ++a;
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+    
+}

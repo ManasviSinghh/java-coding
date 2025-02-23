e428582fd886ae44603eaef9d567b09f7762feb8
Manasvi Singh <2200560100059@bbdniit.ac.in>
2025-02-23 23:53:10 +0530

    Add files via upload

---
+++ code10.java
@@ -0,0 +1,8 @@
+public class code10 {//RELATIONAL OPERATOR
+    public static void main(String[] args) {
+        int A = 4;
+        int B = 5;
+        System.out.println( A == B);
+    }
+    
+}

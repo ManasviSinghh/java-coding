646c897a5ceaf18ba8948bfb747da0172a615adf
Manasvi Singh <2200560100059@bbdniit.ac.in>
2025-02-27 21:02:34 +0530

    Add files via upload

---
+++ code13.java
@@ -0,0 +1,12 @@
+public class code13 {
+    public static void main(String[] args) {
+        int age = 22;
+        if(age >= 18) {
+            System.out.println("Adult : drive , vote");
+        }
+        else{
+            System.out.println("cannot drive");
+        }
+    }
+    
+}

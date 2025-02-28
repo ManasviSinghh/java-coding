d97c52669dc3b1695141cbbd9db097612a04d78a
Manasvi Singh <2200560100059@bbdniit.ac.in>
2025-02-28 21:24:42 +0530

    Add files via upload

---
+++ code14.java
@@ -0,0 +1,16 @@
+public class code14 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 8;
+
+        if(a >= b)
+        {
+        System.out.println("a is largest of 2");
+        }
+        else 
+        {
+        System.out.println("b is larger of 2");    
+        }
+    }
+    
+}

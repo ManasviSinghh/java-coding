27fad4ff5e4c0765d17153831aa13f58c48afc71
Manasvi Singh <2200560100059@bbdniit.ac.in>
2025-03-02 21:40:53 +0530

    Add files via upload

---
+++ code16.java
@@ -0,0 +1,23 @@
+
+
+public class code16 
+{
+    public static void main(String[] args) 
+        {
+        int age = 13;
+        if(age >= 18)
+        {
+        System.out.println("ADULT");
+        }
+        else if(age >= 13 && age <18)
+        {
+        System.out.println("teenager");    
+        }
+        else
+        {
+        System.out.println("CHILD");    
+        }
+        }
+}
+    
+

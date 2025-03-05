29344e1ee432bb7228c01ddc249efc68e6568d4d
Manasvi Singh <2200560100059@bbdniit.ac.in>
2025-03-05 22:27:04 +0530

    Add files via upload

---
+++ code19.java
@@ -0,0 +1,9 @@
+public class code19 {
+    public static void main(String[] args) 
+    {
+    int number = 4;
+    //ternary object
+    String type = ((number%2)==0) ? "even" : "odd";
+    System.out.println(type);    
+    }
+}

8de21b3e52eac4cf0779c0ee334c5e90b5e18a60
Manasvi Singh <2200560100059@bbdniit.ac.in>
2025-03-06 21:16:02 +0530

    Add files via upload

---
+++ code20.java
@@ -0,0 +1,10 @@
+public class code20 
+{
+    public static void main(String[] args) 
+    {
+        int marks = 67;
+
+        String reportCard=  marks >= 33 ? "PASS" : "FAIL" ;
+        System.out.println(reportCard);
+    }
+}

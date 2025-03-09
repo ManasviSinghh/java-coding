23475ca26debda4cc37b1090790570e4798d3f55
Manasvi Singh <2200560100059@bbdniit.ac.in>
2025-03-09 16:57:57 +0530

    Add files via upload

---
+++ code23.java
@@ -0,0 +1,13 @@
+public class code23 
+{
+public static void main(String[] args) 
+{
+int counter = 0;
+while(counter < 10)
+{
+    System.out.println("hello world");
+    counter++;
+}    
+}
+    
+}

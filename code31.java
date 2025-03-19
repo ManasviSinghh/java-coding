cfa487ef3c3cae7470fd6b4204fb2ecb2c3836a8
Manasvi Singh <2200560100059@bbdniit.ac.in>
2025-03-19 08:56:05 +0530

    Add files via upload

---
+++ code31.java
@@ -0,0 +1,11 @@
+public class code31 
+{
+public static void main(String[] args) 
+{
+int counter = 1;
+do { 
+   System.out.println("hello world");
+   counter++; 
+} while (counter <= 10);    
+}    
+}

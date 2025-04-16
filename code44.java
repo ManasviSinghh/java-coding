936fc8124314b6d0c57fb8743b7c4763b46bf1e7
Manasvi Singh <2200560100059@bbdniit.ac.in>
2025-04-16 21:06:39 +0530

    Add files via upload

---
+++ code44.java
@@ -0,0 +1,21 @@
+
+public class code44
+{
+public static void main(String[] args) 
+{ 
+int n =11;
+for (int i = 0; i < n; i++) 
+{
+for (int j = 0; j < n; j++) 
+{
+    if(j-i==(n-1)/2 || i+j==(n-1)/2 || i==(n-1)/2)
+    {
+        System.out.print("*");
+    }
+    else
+    { 
+        System.out.print(" ");
+    } 
+}
+System.out.println();
+}}}
